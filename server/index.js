a18149275e79c79eda0608bf2b05d5d5b026e856
Esutarosa <107695658+Esutarosa@users.noreply.github.com>
2023-08-24 02:18:46 +0300

    db connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 require('dotenv').config()
 
 const express = require('express')
+const sequelize = require('./db')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+const start = async () => {
+    try {
+        await sequelize.authenticate() // db connection
+        await sequelize.sync() // checks db state against db schema
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e);
+    }
+}
+start()
