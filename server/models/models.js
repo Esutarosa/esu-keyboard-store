c1792909d1eb42a8891c8c6948f15332f5e54c6a
Esutarosa <107695658+Esutarosa@users.noreply.github.com>
2023-08-24 21:50:29 +0300

    models

---
+++ server/models/models.js
@@ -0,0 +1,13 @@
+const sequelize = require('../db')
+const { DataTypes } = require('sequelize') // describes field types
+
+const User = sequelize.define('user', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    email: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING },
+    role: { type: DataTypes.STRING, defaultValue: "USER" },
+})
+
+const Basket = sequelize.define('basket', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+})
